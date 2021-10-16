4d39377c1be8b5338bcf10c0139dd5787c1f3fcd
Joyner <qcx60990@gmail.com>
2021-10-16 11:49:22 -0400

    1

--- 1076.sql
+++ 1076.sql
@@ -8,3 +8,14 @@ HAVING COUNT(employee_id) =
   GROUP BY project_id
   ORDER BY nums DESC
   LIMIT 1) 
+
+
+
+
+-- 2nd solution
+# Write your MySQL query statement below
+WITH tb1 AS (SELECT project_id, count(1) AS cnt FROM Project GROUP BY project_id)
+
+SELECT project_id
+FROM tb1
+WHERE cnt = (SELECT MAX(cnt) FROM tb1)  
